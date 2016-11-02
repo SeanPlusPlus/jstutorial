64293a1a4e7fae125e7905933e212da75d26fcfc
Sean Stephenson <sms@pixar.com>
2016-11-02 14:42:09 -0700

    add final step - flow

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,6 +6,7 @@ import eslint from 'gulp-eslint';
 import mocha from 'gulp-mocha';
 import del from 'del';
 import webpack from 'webpack-stream';
+import flow from 'gulp-flowtype';
 import webpackConfig from './webpack.config.babel';
 
 const paths = {
@@ -30,6 +31,7 @@ gulp.task('lint', () =>
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError())
+    .pipe(flow({ abort: true })) // Add Flow here
 );
 
 gulp.task('clean', () => del([
